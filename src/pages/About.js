6536565f30e2e5b23e3c77a954355d0a48f63a15
Arkadiy <ksailens@yandex.ru>
2022-04-03 22:25:54 +0300

    add new models

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,11 @@ export const About = () => {
   return (
     <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container">
-        <h1 className="display-4">Система расчета длительности литотрипсии</h1>
-        <p className="lead">Version 0.1</p>
+        <h4 className="display-6">Система расчета длительности контактной лазерной литотрипсии</h4>
+        <p className="lead">Version 0.2</p>
+        <p className="lead">Разработчики: Герцовская Карина Владиславовна, Чернега Виктор Степанович</p>
+        <p className="lead">Севастопольский государственный университет</p>
+
       </div>
     </div>
   )
